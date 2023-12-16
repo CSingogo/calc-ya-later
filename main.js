632b6d5dee013d46292c0fd43b9eb9a1b4b063aa
CSingogo <singogosingogo@gmail.com>
2023-12-16 21:24:16 +0200

    created buttons

---
+++ main.js
@@ -0,0 +1,26 @@
+function add(x,y){
+       x + y;
+}
+
+function subtract(x,y){
+    x - y;
+}
+
+function multiply(x,y){
+    x * y;
+}
+
+function divide(x,y){
+    x / y;
+}
+
+// variables
+let num1;
+let num2;
+let operator;
+
+//operate function
+function operate(a,opp,c){
+    
+
+}
