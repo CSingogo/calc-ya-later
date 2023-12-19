c71205f1d7f288c856cbd6835df89540c86b1e23
CSingogo <singogosingogo@gmail.com>
2023-12-19 21:01:15 +0200

    added logic for displaying values

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function add(x,y){
-       x + y;
+       return x + y;
 }
 
 function subtract(x,y){
-    x - y;
+    return x - y;
 }
 
 function multiply(x,y){
-    x * y;
+   return x * y;
 }
 
 function divide(x,y){
-    x / y;
+   return x / y;
 }
 
 // variables
@@ -20,7 +20,68 @@ let num2;
 let operator;
 
 //operate function
-function operate(a,opp,c){
+function operate(num1,operator,num2){
+    if (operator === '+')
+      {
+        document.getElementById('display').innerText = add(num1,num2);
+      }
+      else if(operator === '-')
+         {
+            document.getElementById('dispaly').innerText = subtract(num1,num2);
+         }
+           else if(operator === '*')
+            {
+                document.getElementById('display').innerText =  multiply(num1,num2);
+            }
+             else
+               {
+                document.getElementById('display').innerText =  divide(num1,num2);
+               }
     
 
+}
+//logic for the display
+let currentInput = '0';
+
+function appendToDisplay(value) {
+
+    let parsed = parseInt(value);
+     
+    if (!isNaN(parsed) && typeof parsed === 'number') {
+          if(num1)
+            {
+                num2 = parsed;
+            }
+            else
+            {
+                num1 = parsed;
+            }
+      } else {
+        operator = parsed;
+      }
+
+
+
+    if (currentInput === '0') {
+        currentInput = value;
+    } else {
+        currentInput += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '0';
+    updateDisplay();
+    num1 = false;
+}
+
+function calculateResult() {
+    // Implement your calculation logic here
+    // For example: currentInput = eval(currentInput);
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').innerText = currentInput;
 }
